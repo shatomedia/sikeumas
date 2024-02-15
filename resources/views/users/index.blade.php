2a3264235b79c7dac98e7cc0efdd837775bd9866
faik-irkham <faikirkham@gmail.com>
2024-02-16 05:09:41 +0700

    add view index in users core app

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row mb-3">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Data Staff</h3>
+
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Data Staff</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <!-- Basic Tables start -->
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    <a href="{{ route('users.create') }}" class="btn btn-primary float-end">Tambah Staff</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table" id="table1">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Alamat</th>
+                                    <th>Email</th>
+                                    <th>No Hp</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->alamat }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->telp }}</td>
+                                        <td class="d-flex">
+                                            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-sm btn-primary"
+                                                style="margin-right: 5px">Edit</a>
+                                            @if (auth()->user()->id != $user->id)
+                                                <form action="" method="POST">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <input type="submit" class="btn btn-sm btn-secondary" value="Hapus">
+
+                                                </form>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- Basic Tables end -->
+
+    </div>
+@endsection

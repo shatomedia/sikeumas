ed95ad7d8d3b6743ee6ec9cd59341ebb7d61897e
faik-irkham <faikirkham@gmail.com>
2024-02-16 05:07:06 +0700

    edit user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,10 +16,30 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $admin = User::create([
             'name' => 'Shatomedia',
             'email' => 'shatomedia@gmail.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
         ]);
+
+        $reseller = User::create([
+            'name' => 'Reseller',
+            'email' => 'reseller@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $role = Role::create(['name' => 'staff']);
+        $role = Role::create(['name' => 'reseller']);
+
+        $permission = Permission::create(['name' => 'create user']);
+        $permission = Permission::create(['name' => 'read user']);
+        $permission = Permission::create(['name' => 'update user']);
+        $permission = Permission::create(['name' => 'delete user']);
+
+        $admin->assignRole('admin');
+        $reseller->assignRole('reseller');
     }
 }

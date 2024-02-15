84b175ccc5f6c1d6c4e4e31a944714a4b13191c4
faik-irkham <faikirkham@gmail.com>
2024-02-16 05:08:43 +0700

    add view reseller in core app

---
+++ resources/views/reseller/index.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row mb-3">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Data Resellers</h3>
+
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Data Resellers</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <!-- Basic Tables start -->
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    <a href="{{ route('resellers.create') }}" class="btn btn-primary float-end">Tambah Resellers</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table" id="table1">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Alamat</th>
+                                    <th>Email</th>
+                                    <th>No Hp</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($resellers as $reseller)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $reseller->name }}</td>
+                                        <td>{{ $reseller->alamat }}</td>
+                                        <td>{{ $reseller->email }}</td>
+                                        <td>{{ $reseller->telp }}</td>
+                                        <td class="d-flex">
+                                            <a href="" class="btn btn-sm btn-primary"
+                                                style="margin-right: 5px">Edit</a>
+                                            <form action="" method="POST">
+                                                @method('DELETE')
+                                                @csrf
+                                                <input type="submit" class="btn btn-sm btn-secondary" value="Hapus">
+
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- Basic Tables end -->
+
+    </div>
+@endsection

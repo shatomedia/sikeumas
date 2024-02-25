ad59eb525a668a04fece359e12474352fb930561
faik-irkham <faikirkham@gmail.com>
2024-02-25 16:25:43 +0700

    add permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('permissions')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $file = database_path('import/permissions.csv');
+        $handle = fopen($file, "r");
+
+        if ($handle) {
+            $header = fgetcsv($handle, 1000, ",");
+            while (($data = fgetcsv($handle, 1000, ",", '"')) !== FALSE) {
+                DB::table('permissions')->insert([
+                    'name' => $data[0],
+                    'guard_name' => 'web',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+            fclose($handle);
+        }
+    }
+}

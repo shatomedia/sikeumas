34e2c70a36f4824a2ff96ac8851c56daf04cf480
faik-irkham <faikirkham@gmail.com>
2024-02-25 16:26:01 +0700

    sesuaikan controller

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -38,14 +38,6 @@ class PermissionController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(Permission $permission)

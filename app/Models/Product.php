b54c387b0a3cf9ae33f4c50574a12880c1370383
faik-irkham <faikirkham@gmail.com>
2024-03-03 02:02:28 +0700

    product fitur

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\GenerateSlug;
 use App\Traits\HasCreatedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    use HasCreatedBy;
+    use HasCreatedBy, GenerateSlug;
     protected $guarded = [];
 }
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Tambah Acara / Agenda</h3>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Tambah Acara</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="section">
+        <div class="card">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <div class="card-body">
+                <form class="form form-horizontal" action="{{ route('produk.store') }}" method="POST"
+                    enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="nama">Nama Produk</label>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <input type="text" id="nama" class="form-control mb-3" name="nama"
+                                    placeholder="Nama Produk">
+                            </div>
+                            <div class="col-md-12">
+                                <label for="deskripsi">Deskripsi</label>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3"></textarea>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="spesifikasi">Spesifikasi</label>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <textarea class="form-control" id="summernote" name="spesifikasi"></textarea>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="foto">Gambar</label>
+                            </div>
+                            <div class="col-12 col-md-6 form-group">
+                                <input type="file" id="foto" name="foto" class="image-preview-filepond">
+                            </div>
+
+                            <div class="col-sm-12 d-flex justify-content-start">
+                                <button type="submit" class="btn btn-primary me-1 mb-1">Simpan</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection

2fab34f79cd7db5e26ca9a77aae3d01b7628ae57
faik-irkham <faikirkham@gmail.com>
2024-03-05 06:19:51 +0700

    edit landing controller

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -10,6 +10,6 @@ class LandingController extends Controller
     public function index()
     {
         $data['bestProducts'] = Product::get()->take(3);
-        return view('welcome', $data);
+        return view('company-theme.welcome', $data);
     }
 }

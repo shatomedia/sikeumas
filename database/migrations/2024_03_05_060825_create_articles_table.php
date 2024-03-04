53827b06a661fcb6ffcd98d8cc85eedf49fbd074
faik-irkham <faikirkham@gmail.com>
2024-03-05 06:20:27 +0700

    add file for blog fitur

---
+++ database/migrations/2024_03_05_060825_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->index();
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->text('konten');
+            $table->string('gambar');
+            $table->string('penulis');
+            $table->integer('views')->default(0);
+            $table->string('status');
+            $table->date('publish_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};

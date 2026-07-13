bb0f8db26458db8fcac7030a84fe9b614ce0c623
Rhosyad <49601380+shatomedia@users.noreply.github.com>
2026-07-13 23:21:56 +0700

    Enable the 'Masuk' (login) button in the public nav, was left commented out

--- resources/views/layouts/company_navbar.blade.php
+++ resources/views/layouts/company_navbar.blade.php
@@ -49,11 +49,11 @@
                     Hubungi Kami: 085743909116
                 </a>
             </div>
-            {{-- <div class="btn-login mx-2">
+            <div class="btn-login mx-2 d-none d-lg-block">
                 <a href="{{ route('login') }}" class="btn w-100" id="btn-login">
                     Masuk
                 </a>
-            </div> --}}
+            </div>
         </div>
     </div>
 </nav>

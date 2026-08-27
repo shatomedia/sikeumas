7f9875330123ff6b6ddf2e7e63a027c11d1cbc6b
Rhosyad <49601380+shatomedia@users.noreply.github.com>
2026-08-27 07:21:28 +0700

    Change buku-teknologi landing URL from /buku-teknologi to /buku

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/tentang-kami', [LandingController::class, 'aboutUs'])->name('about-
 Route::get('/taqwa-hub', [LandingController::class, 'taqwaHub'])->name('taqwa-hub-landing');
 Route::get('/surgery-time', [LandingController::class, 'surgeryTime'])->name('surgery-time-landing');
 Route::get('/jws-m3', [LandingController::class, 'jwsM3'])->name('jws-m3-landing');
-Route::get('/buku-teknologi', [LandingController::class, 'bukuTeknologi'])->name('buku-teknologi-landing');
+Route::get('/buku', [LandingController::class, 'bukuTeknologi'])->name('buku-teknologi-landing');
 Route::get('/produk', [LandingController::class, 'product'])->name('product');
 Route::get('/produk/{slug}', [LandingController::class, 'productDetail'])->name('product-detail');
 Route::get('/artikel', [LandingController::class, 'article'])->name('blog');
